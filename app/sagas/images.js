eccf85ccf63f76e310cb5d2637655e1feea28cc1
sharif hossain <sharif@myego2go.de>
2022-07-24 19:21:36 +0600

    saga file updated

--- app/sagas/images.js
+++ app/sagas/images.js
@@ -1,14 +1,10 @@
+import { takeLatest} from 'redux-saga/effects';
+import {FETCH_IMAGES_SUCCESS} from '../actionType/image';
 
-import {  select, put, takeEvery, takeLatest } from "redux-saga/effects";
-import {
-    FETCH_IMAGES_SUCCESS,
-  } from "../actionType/image";
-
-  function* getResult(){
-    console.log('you have successfully got images saga 8');
-  }
+function* getResult() {
+  console.log('you have successfully got images');
+}
 
 export function* watchImages() {
-    yield takeLatest(FETCH_IMAGES_SUCCESS, getResult);
-  }
-  
+  yield takeLatest(FETCH_IMAGES_SUCCESS, getResult);
+}
